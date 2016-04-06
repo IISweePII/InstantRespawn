44509842c6861180f1b905d238957d7b0591eaeb
thebigsmileXD <thebigsmilexd@gmail.com>
2016-04-06 15:31:20 +0200

    Reupload

---
+++ README.md
@@ -0,0 +1,33 @@
+# InstantRespawn
+Players respawn instantly or with delay in defined worlds
+
+## About
+This plugin was made by @thebigsmileXD, because he (me) read about players asking "can we respawn players instantly" in  PocketMine forums. The answer is YES!
+
+With this plugin you are able to let players respawn instantly when they die and also cancel move for * seconds. You can even define if the players will drop their stuff or not, and if they should always spawn in one world or the one they died in!
+
+## How to use this plugin
+Download the latest version of this plugin [here](http://5.175.226.133/de_DE/plugins/index.php).
+
+Drop this plugin into your plugins folder.
+
+Set the worlds where you want to use instantrespawn via */instantrespawn add <worldname>*
+
+The players will die and respawn instantly.
+
+## Features
+- configure respawn speed (also 0 (zero) possible)
+- enable/disable hide player
+- enable/disable drops
+- enable/disable death messages
+- respawn in 1 set world or in the same
+- resets all effects and extinguish players
+- every message changeable
+
+## To Do
+- blindness in 0.12.x/0.13.x
+- add second version for 0.12.x/0.13.x to reset hunger
+
+## Notes
+
+This plugin was coded and is maintained by @thebigsmileXD of the Imagical Corporation. This is not a plugin for everyone, this is a plugin for our intended use. Hopefully you find it useful too.
---
+++ src/thebigsmileXD/InstantRespawn/SendTip.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace thebigsmileXD\InstantRespawn;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
+use pocketmine\block\Block;
+use pocketmine\entity\Entity;
+use pocketmine\nbt\tag\Compound;
+use pocketmine\nbt\tag\Enum;
+use pocketmine\nbt\tag\Double;
+use pocketmine\nbt\tag\Float;
+use pocketmine\nbt\tag\Int;
+use pocketmine\nbt\tag\Byte;
+use pocketmine\Player;
+
+class SendTip extends PluginTask{
+
+	public function __construct(Plugin $owner, $player){
+		parent::__construct($owner);
+		$this->plugin = $owner;
+		$this->player = $player;
+	}
+
+	public function onRun($currentTick){
+		if($this->player instanceof Player && $this->getOwner()->getServer()->getPlayer($this->player->getName()) !== null){
+			$player = $this->getOwner()->getServer()->getPlayer($this->player->getName());
+			$player->sendTip(str_replace("{PLAYER}", $player->getDisplayName(), TextFormat::RED . $this->getOwner()->getConfig()->getNested("messages.messagetoplayer")));
+			/*
+			 * $fall = Entity::createEntity("FallingSand", $player->getLevel()->getChunk($player->x >> 4, $player->z >> 4), new Compound("", ["Pos" => new Enum("Pos", [new Double("", $player->x),new Double("", $player->y + 1),new Double("", $player->z)]),
+			 * "Motion" => new Enum("Motion", [new Double("", 0),new Double("", 0),new Double("", 0)]),"Rotation" => new Enum("Rotation", [new Float("", 0),new Float("", 0)]),"TileID" => new Int("TileID", Block::STAINED_HARDENED_CLAY),"Data" => new Byte("Data", 15)]));
+			 * $fall->spawnTo($player);
+			 */
+			return;
+		}
+		else
+			// if(isset($fall) && $fall !== null) $fall->despawnFromAll();
+			$this->getOwner()->getServer()->getScheduler()->cancelTask($this->getTaskId());
+		return;
+	}
+}
+?>
